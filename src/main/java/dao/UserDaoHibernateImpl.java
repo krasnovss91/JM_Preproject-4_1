77f864ddee6592729f71aa41ac4b6c46b7747407
krasnovss91 <krasnovstanislav91@gmail.com>
2020-06-04 16:07:24 +0400

    Hibernate_1

--- src/main/java/dao/UserDaoHibernateImpl.java
+++ src/main/java/dao/UserDaoHibernateImpl.java
@@ -17,70 +17,107 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsersDao() throws HibernateException {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        List<User> allUsers = session.createQuery("FROM User").list();
-        try{
-            transaction.commit();
+        //  Session session = sessionFactory.openSession();
+        //    Transaction transaction = session.beginTransaction();
+        //   List<User> allUsers = session.createQuery("FROM User").list();
+        Session session = null;
+        Transaction transaction = null;
+        List<User> allUsers = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            allUsers = session.createQuery("FROM User").list();
+            //     transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
         } finally {
             session.close();
-            return allUsers;
+            //    return allUsers;
         }
+        return allUsers;
     }
 
     @Override
     public User getUserByIdDao(long id) throws HibernateException {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from User where id = :userId");
-        List<User> userList = query.setParameter("userId", id).list();
-        User user = userList.get(0);
-        try{
-            transaction.commit();
+        // Session session = sessionFactory.openSession();
+        // Transaction transaction = session.beginTransaction();
+        //  Query query = session.createQuery("from User where id = :userId");
+        //  List<User> userList = query.setParameter("userId", id).list();
+        //  User user = userList.get(0);
+        Session session = null;
+        Transaction transaction = null;
+        Query query;
+        List<User> userList;
+        User user = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            query = session.createQuery("from User where id = :userId");
+            //   transaction.commit();
+            userList = query.setParameter("userId", id).list();
+            user = userList.get(0);
         } catch (HibernateException e) {
             transaction.rollback();
         } finally {
             session.close();
-            return user;
+            // return user;
         }
+        return user;
     }
 
     @Override
     public boolean checkUserByNameDao(String name) throws HibernateException {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from User where name = :userName");
-        List<User> userList = query.setParameter("userName", name).list();
+        // Session session = sessionFactory.openSession();
+        //  Transaction transaction = session.beginTransaction();
+        // Query query = session.createQuery("from User where name = :userName");
+        //  List<User> userList = query.setParameter("userName", name).list();
+        Session session = null;
+        Transaction transaction = null;
+        Query query;
+        List<User> userList = null;
         try {
-            transaction.commit();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            query = session.createQuery("from User where name = :userName");
+            userList = query.setParameter("userName", name).list();
+            // transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
         } finally {
             session.close();
-            if (userList.size() > 0) {
-                return false;
-            } else return true;
+            //  if (userList.size() > 0) {
+            //      return false;
+            //  } else return true;
         }
+        if (userList.size() > 0) {
+            return false;
+        } else return true;
     }
 
     @Override
     public boolean checkUserByLoginDao(String login) throws HibernateException {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from User where login = :userLogin");
-        List<User> userList = query.setParameter("userLogin", login).list();
+        //  Session session = sessionFactory.openSession();
+        // Transaction transaction = session.beginTransaction();
+        //  Query query = session.createQuery("from User where login = :userLogin");
+        //  List<User> userList = query.setParameter("userLogin", login).list();
+        Session session = null;
+        Transaction transaction = null;
+        Query query;
+        List<User> userList = null;
         try {
-            transaction.commit();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            query = session.createQuery("from User where login = :userLogin");
+            userList = query.setParameter("userLogin", login).list();
+            //  transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
         } finally {
             session.close();
-            if (userList.size() > 0) {
-                return false;
-            } else return true;
         }
+        if (userList.size() > 0) {
+            return false;
+        } else return true;
     }
 
     @Override
@@ -136,25 +173,35 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public User isExist(String login, String password) throws HibernateException {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from User where login = :userLogin");
-        List<User> userList = query.setParameter("userLogin", login).list();
-
+        //  Session session = sessionFactory.openSession();
+        //  Transaction transaction = session.beginTransaction();
+        //  Query query = session.createQuery("from User where login = :userLogin");
+        //  List<User> userList = query.setParameter("userLogin", login).list();
+        Session session = null;
+        Transaction transaction = null;
+        Query query;
+        List<User> userList;
         User userExist = null;
-        for (User user : userList) {
-            if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
-                userExist = user;
-            }
-        }
         try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            query = session.createQuery("from User where login = :userLogin");
+            userList = query.setParameter("userLogin", login).list();
+
+            for (User user : userList) {
+                if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
+                    userExist = user;
+                }
+            }
+            //  try {
             transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
         } finally {
             session.close();
-            return userExist;
+            //   return userExist;
         }
+        return userExist;
     }
 
 }
